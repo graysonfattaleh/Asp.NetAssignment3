8f095a862f578b6c3430045733ed4a1b86578475
graysonfattaleh <71087950+graysonfattaleh@users.noreply.github.com>
2021-03-22 17:45:44 -0600

    retain rating and stuf chnges

---
+++ HiltonBrotherMovieDB/wwwroot/js/scriptz.js
@@ -0,0 +1,40 @@
+﻿
+
+
+
+
+function set_default_vals() {
+
+    let ratings = document.getElementById("rating");
+    let edit_statuses = document.getElementById("edited");
+    let current_rating = document.getElementById("rating_hidden")
+    let current_edit = document.getElementById("edited_hidden")
+
+  
+
+
+    for (iCount = 0; iCount < ratings.options.length; iCount++) {
+       
+        if (current_rating.value == ratings.options[iCount].value) {
+            ratings.options[iCount].selected = true;
+        };
+    };
+
+    // ccheck status
+    if (current_edit.value == "value") {
+        current_edit = "True"
+    }
+    else {
+        current_edit = "False"
+    }
+
+    for (iCount = 0; iCount < edit_statuses.options.length; iCount++) {
+       
+        if (current_edit == edit_statuses.options[iCount].value) {
+            edit_statuses.options[iCount].selected = true;
+        };
+    };
+   
+};
+
+set_default_vals()
